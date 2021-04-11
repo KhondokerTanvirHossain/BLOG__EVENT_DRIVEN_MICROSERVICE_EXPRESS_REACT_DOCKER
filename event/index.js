e50333f47d305fb01c9e9d6b6c28b25d455865ab
K. Tanvir Hossain <tanvir@cokreates.com>
2021-04-12 01:39:01 +0600

    event queue

--- event/index.js
+++ event/index.js
@@ -9,9 +9,17 @@ app.use(express.json());
 
 app.use(cors());
 
+const events = [];
+
+app.get('/events', (req, res) => {
+    res.send(events);
+});
+
 app.post('/events', (req, res) => {
     const event = req.body;
     
+    events.push(event);
+
     axios.post('http://localhost:8080/events', event);
     axios.post('http://localhost:8081/events', event);
     axios.post('http://localhost:8082/events', event);
--- query/index.js
+++ query/index.js
@@ -2,6 +2,7 @@
 const { v4: uuidv4 } = require('uuid');
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 
@@ -11,39 +12,49 @@ app.use(cors());
 
 var posts = {};
 
+const handleEvent = (type, data) => {
+    if (type == 'Post') {
+        const id = uuidv4();
+        const content = data.content;
+        posts[id] = ({ id, content });
+
+    } else if (type == 'Comment') {
+        const id = uuidv4();
+        const comment = data.comment;
+        const status = data.status;
+        post = posts[data.id];
+        console.log(post);
+        if (!("comments" in post)) {
+            post["comments"] = [];
+        }
+        post.comments.push({ id, comment, status });
+    }
+
+    else if (type == 'Approval') {
+        post = posts[data.id];
+        console.log(post);
+        post.comments.filter(c => {
+            c.status = data.status;
+        });
+    }
+};
+
 app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
 app.post('/events', (req, res) => {
 
-    if (req.body.type == 'Post') {
-        const id = uuidv4();
-        const content = req.body.data.content;
-        posts[id] = ({ id, content });
-
-    } else if (req.body.type == 'Comment') {
-        const id = uuidv4();
-        const comment = req.body.data.comment;
-        const status = req.body.data.status;
-        post = posts[req.body.data.id];
-        console.log(post);  
-        if (!("comments" in post)) {
-            post["comments"] = [];
-        }
-        post.comments.push({id, comment, status});
-    }
-
-    else if (req.body.type == 'Approval') {
-        post = posts[req.body.data.id];
-        console.log(post);
-        post.comments.filter(c => {
-            c.status = req.body.data.status;
-        });
-    }
+    handleEvent(req.body.type, req.body.data);
+    
     res.send({});
 });
 
-app.listen(8082, () => {
-
+app.listen(8082, async () => {
+    
+   const res = await axios.get('http://localhost:8085/events');
+   for (let event of res.data) {
+        console.log("Processing Event : " + event.type);
+        handleEvent(event.type, event.data);
+   }
 });
